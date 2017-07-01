0f63930aafae5fae8a2d745a14a62598db8ba52a
mortuie <jimmybob-leon@hotmail.co.uk>
2017-07-01 22:48:33 +0100

    Working on the LocalWeatherApplication

--- LocalWeatherApp/apirequests.js
+++ LocalWeatherApp/apirequests.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
 
 	function getLocation() {
 		if (navigator.geolocation) {
-			x.text("testing");
 			navigator.geolocation.getCurrentPosition(showPosition);
 		} else {
 			x.text("Geolocation is not supported by this browser>");
@@ -15,10 +14,7 @@ $(document).ready(function() {
 	}
 
 	function showPosition(position) {
-		var latitude = position.coords.latitude;
-		var longitude = position.coords.longitude;
-
-		getData(latitude, longitude);
+		getData(position.coords.latitude, position.coords.longitude);
 	}
 
 	function getData(latitude, longitude) {
@@ -27,12 +23,42 @@ $(document).ready(function() {
 			url: "https://api.darksky.net/forecast/" + config.apiKey + "/" + latitude + "," + longitude,
 		}).done(function(data) {
 			console.log(data);
+			var temperature = data.currently.temperature;
+			var windSpeed = data.currently.windSpeed;
+			var where = data.currently.timezone;
+			var summary = data.currently.summary;
+
+			setData(temperature, windSpeed, where, summary);
 		})
 	}
 
+	function setData(temp, wind, where, summary) {
+		$("#temperature").text(Math.round(temp).toString() + " F");
+		$("#wind").text(wind);
+		$("#location").text(where);
+		$("#summary").text(summary);
+	}
 
 
+	function getFarenheitFromDegrees(degrees) {
+		return Math.round((parseInt(degrees) * 1.8)) + 32;
+	}
 
+	function getDegreesFromFarenheit(farenheit) {
+		return Math.round(((parseInt(farenheit) - 32) / 1.8)).toString();
+	}
+
+	$("#convert").on("click", function() {
+		var currentTemperature = $("#temperature").text();
+
+		if (currentTemperature.indexOf("F") > -1) {
+			$("#temperature").text(getDegreesFromFarenheit(currentTemperature.replace(" F", "")) + " C");
+			$("#convert").text("Convert to Farenheit");
+		} else {
+			$("#temperature").text(getFarenheitFromDegrees(currentTemperature.replace(" C", "")) + " F");
+			$("#convert").text("Convert to Celcius");
+		}
+	});
 
 });
 
