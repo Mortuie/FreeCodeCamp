9cc8891ede1867fc83c2476dd54a67bc7d4ef2cd
mortuie <jimmybob-leon@hotmail.co.uk>
2017-07-04 23:21:19 +0100

    Putting data received back into a useable object

--- WikipediaViewer/script.js
+++ WikipediaViewer/script.js
@@ -1,33 +1,47 @@
 $(document).ready(function() {
-	var search = "";
+	var wikiUrl = "https://en.wikipedia.org/w/api.php";
+	var arrayOfData = [];
+
 
 	$("#pressTest").on("click", function() {
 		window.open("https://en.wikipedia.org/wiki/Special:Random");
 	});
 
 	$("#searchWiki").on("click", function() {
+		console.log($("#searchPhrase").val());
+		wikiUrl += "?" + $.param({
+			"action": "opensearch",
+			"search": $("#searchPhrase").val(),
+			"prop": "revisions",
+			"rvprop": "content",
+			"format": "json",
+			"limit": 20,
+		})
+
+
+		$.ajax({
+		url: wikiUrl,
+		dataType: "jsonp",
+		success: function(data) {
+
+			console.log(data);
+			for (var i = 0; i < data[1].length; i++) {
+				var temp = {};
+				temp.title = data[1][i];
+				temp.description = data[2][i];
+				temp.link = data[3][i];
+				arrayOfData.push(temp);
+			}
+			console.log(arrayOfData);
+		}});
 
 	});
 
 	
-	var wikiUrl = "https://en.wikipedia.org/w/api.php";
-	wikiUrl += "?" + $.param({
-		"action": "opensearch",
-		"search": "",
-		"prop": "revisions",
-		"rvprop": "content",
-		"format": "json",
-		"limit": 20,
-	})
+	
 
 
-	$.ajax({
-		url: wikiUrl,
-		dataType: "jsonp",
-		success: function(data) {
-			console.log(data[3]);
-		}
-	});
+	
 
 
 });
