7cf96f416af4ed985f49812f3b9ecbfa15006e68
mortuie <jimmybob-leon@hotmail.co.uk>
2017-07-04 22:55:57 +0100

    Adding search functionality

--- WikipediaViewer/script.js
+++ WikipediaViewer/script.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
+	var search = "";
 
 	$("#pressTest").on("click", function() {
 		window.open("https://en.wikipedia.org/wiki/Special:Random");
+	});
+
+	$("#searchWiki").on("click", function() {
+
 	});
 
 	
 	var wikiUrl = "https://en.wikipedia.org/w/api.php";
 	wikiUrl += "?" + $.param({
 		"action": "opensearch",
-		"search": "butterflies",
+		"search": "",
 		"prop": "revisions",
 		"rvprop": "content",
 		"format": "json",
-		"limit": 10,
+		"limit": 20,
 	})
 
 
@@ -20,7 +25,7 @@ $(document).ready(function() {
 		url: wikiUrl,
 		dataType: "jsonp",
 		success: function(data) {
-			console.log(data);
+			console.log(data[3]);
 		}
 	});
 
