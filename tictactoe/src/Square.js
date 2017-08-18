89389c5f8aaa3ce791f2e183be51b230c2716026
mortuie <jimmybob-leon@hotmail.co.uk>
2017-08-18 14:01:04 +0100

    Adding some more code to the game

--- tictactoe/src/Square.js
+++ tictactoe/src/Square.js
@@ -4,12 +4,20 @@ import './index.css';
 
 export default class Square extends React.Component {
 
+	constructor() {
+		super();
+		this.state = {
+			value: null,
+		};
+	}
+
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
-  
+
 }
