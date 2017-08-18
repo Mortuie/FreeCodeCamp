2908a4365b0c399f1ba53a72638522a5edaffffd
mortuie <jimmybob-leon@hotmail.co.uk>
2017-08-18 12:38:43 +0100

    Refactoring the code

---
+++ tictactoe/src/Game.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Board from './Board.js';
+
+export default class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ tictactoe/src/Square.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+export default class Square extends React.Component {
+
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+  
+}
