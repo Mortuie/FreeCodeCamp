b55317fbc4ed170eba9bdc3eed0ae5f4a98edeee
mortuie <jimmybob-leon@hotmail.co.uk>
2017-08-20 08:40:24 +0100

    Making good progress on the REACTjs game

--- tictactoe/src/Board.js
+++ tictactoe/src/Board.js
@@ -5,8 +5,26 @@ import Square from './Square.js';
 
 export default class Board extends React.Component {
   
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return ( 
+      <Square
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)} 
+      />
+    );
   }
 
   render() {
--- tictactoe/src/Square.js
+++ tictactoe/src/Square.js
@@ -4,18 +4,10 @@ import './index.css';
 
 export default class Square extends React.Component {
 
-	constructor() {
-		super();
-		this.state = {
-			value: null,
-		};
-	}
-
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
