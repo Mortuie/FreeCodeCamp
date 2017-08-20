98e7b5acf76044129e74fd0d3acf97d6cdc9612f
mortuie <jimmybob-leon@hotmail.co.uk>
2017-08-20 09:34:15 +0100

    Added some more changes so that players now take turn

--- tictactoe/src/Board.js
+++ tictactoe/src/Board.js
@@ -9,13 +9,17 @@ export default class Board extends React.Component {
     super();
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -28,7 +32,7 @@ export default class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player:' + (this.state.xIsNext ? "X" : "O");
 
     return (
       <div>
