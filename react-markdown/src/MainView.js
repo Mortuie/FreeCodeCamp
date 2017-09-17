e1623181223181467d5b5b9c6bef90060a33b23a
mortuie <jimmybob-leon@hotmail.co.uk>
2017-09-17 22:33:12 +0100

    Adding the start of the REACT-MARKDOWN project

---
+++ react-markdown/src/MainView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class MainView extends React.Component {
+
+
+	render() {
+ 		return <h1>Hello World!</h1>;
+    	}
+
+}
+
---
+++ react-markdown/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainView from './MainView';
+
+ReactDOM.render(
+	<MainView />,
+	document.getElementById('root')
+);
+
