b58aecab7d50a6a3260dc5dd00b48604f827f8c8
mortuie <jimmybob-leon@hotmail.co.uk>
2017-09-17 14:04:09 +0100

    Adding more stuff to the SimonGame

--- SimonGame/scripts.js
+++ SimonGame/scripts.js
@@ -1,17 +1,81 @@
 $(document).ready(function() {
+	var options = {
+		0: 'red',
+		1: 'blue',
+		2: 'yellow',
+		3: 'green',
+	};
 	var on = false;
-
+	var finished = false;
+	var pastTurns = [];
+	var turns = 0;
 
 
 	$("#onoff").click(function() {
 		if (!on) {
-			$("#onoff").text("turn off");
+			$("#onoff").css("background-color", "green");
 			on = true;
 		} else {
-			$("#onoff").text("turn on");
+			$("#onoff").css("background-color", "red");
 			on = false;
 		}
 	});
 
+	$("#start").click(function() {
+		if (!on) {
+			$("#status").text("Machine is not on!");
+		} else {
+			startGame();
+		}
+	});
+
+	function startGame() {
+		pastTurns = [];
+		var whatTurn = 0;
+		$("#status").text("Starting. Count: 0");
+
+
+		while (!finished) {
+			
+
+		}
+	}
+
+	function computerTurn() {
+
+	};
+
+	function playerTurn() {
+
+	};
+
+	$("#red").click(function() {
+		$(this).addClass("active");
+		setTimeout(function() {
+			$(".active").removeClass("active");
+		}, 500);
+
+	});
+
+	$("#blue").click(function() {
+		$(this).addClass("active");
+		setTimeout(function() {
+			$(".active").removeClass("active");
+		}, 500);
+	});
+
+	$("#yellow").click(function() {
+		$(this).addClass("active");
+		setTimeout(function() {
+			$(".active").removeClass("active");
+		}, 500);
+	});
+
+	$("#green").click(function() {
+		$(this).addClass("active");
+		setTimeout(function() {
+			$(".active").removeClass("active");
+		}, 500);
+	});
 
 });
