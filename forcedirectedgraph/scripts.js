b1a5e9e5f1605d9975ba0c752a7611d0f316f6df
mortuie <jimmybob-leon@hotmail.co.uk>
2017-11-09 11:17:55 +0000

    Adding the drag and drop functionality to the force directed graph

--- forcedirectedgraph/scripts.js
+++ forcedirectedgraph/scripts.js
@@ -8,9 +8,28 @@ var svg = d3.select("div")
 	.attr("height", height);
 
 var simulation = d3.forceSimulation()
-	.force("charge", d3.forceManyBody().strength(-5))
+	.force("charge", d3.forceManyBody().strength(-10))
 	.force("center", d3.forceCenter(width / 2, height / 2))
 	.force("link", d3.forceLink().id(link => link.id).strength(link => link.strength));
+
+
+const dragDrop = d3.drag()
+	.on("start", node => {
+		node.fx = node.x;
+		node.fy = node.y;
+	})
+	.on("drag", node => {
+		simulation.alphaTarget(0.7).restart();
+		node.fx = d3.event.x;
+		node.fy = d3.event.y;
+	})
+	.on("end", node => {
+		if (!d3.event.active) {
+			simulation.alphaTarget(0);
+		}
+		node.fx = null;
+		node.fy = null;
+	});
 
 d3.json("https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json", (error, data) => {
 	if (error) throw error;
@@ -27,8 +46,10 @@ d3.json("https://raw.githubusercontent.com/DealPete/forceDirected/master/countri
 		.enter()
 		.append("svg:image")
 		.attr("xlink:href", d => "flags/" + d.code + ".png")
-		.attr("width", 10)
-		.attr("height", 10);
+		.attr("width", 20)
+		.attr("height", 20);
+
+	nodeElements.call(dragDrop);
 
 	const textElements = svg.append("g")
 		.selectAll("text")
@@ -37,7 +58,7 @@ d3.json("https://raw.githubusercontent.com/DealPete/forceDirected/master/countri
 		.append("text")
 		.text(node => node.country)
 		.attr("font-size", 15)
-		.attr("dx", 15)
+		.attr("dx", 20)
 		.attr("dy", 15);
 
 	const linkElements = svg.append("g")
