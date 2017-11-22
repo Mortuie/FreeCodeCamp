1a9bf1ad46f9dd6aa6acd8f788912ab3e0909859
mortuie <jimmybob-leon@hotmail.co.uk>
2017-11-22 21:57:56 +0000

    Basically completed the image search api...

--- imagesearch/server.js
+++ imagesearch/server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var app = express();
 var env = require("./env.js");
 var request = require("request");
+var mongo = require("mongodb").MongoClient;
 const PORT = 8000;
 
 
@@ -22,32 +23,74 @@ app.get("/api/imagesearch/*", (req, res) => {
 		offset = 10;
 	}
 
-	console.log("offset: " + offset + " searchString: " + searchString);
 
 	var options = {
 		url: "https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=" + searchString + "&count=" + offset,
 		headers: {
-			"Ocp-Apim-Subscription-Key": "08bdd757cb144c2188c59737eebc2a90"
+			"Ocp-Apim-Subscription-Key": env.api_key
 		}
 	};
 
-		
+	request(options, (err, response, body) => {
+		if (err) return res.send("Problem Connection to the API");
 
-	request(options, (err, res, body) => {
-		if (!err && res.statusCode == 200) {
-			var info = JSON.parse(body);
-			console.log(info.value);
+		if (!err && response.statusCode == 200) {
+			var info = JSON.parse(body).value;
+
+			mongo.connect(env.mongo_url, (err, db) => {
+				if (err) return res.send("Error connecting to mongodb....");
+
+
+				var con = db.collection("lastsearches");
+
+				con.insert({searchTerm: searchString, time: new Date()});
+				db.close();
+			});
+
+			var results = [];
+
+			for (var i = 0; i < info.length; i++) {
+				var temp = {};
+				temp.snippet = info[i].name;
+				temp.url = info[i].webSearchUrl;
+				temp.thumbnail = info[i].thumbnailUrl;
+				results.push(temp);
+			}
+
+			res.json(results);
+
+
+		} else {
+			return res.send("Didn't get a 200 status code...");
 		}
+	});
+
+
+});
+
+app.get("/api/latest", (req, res) => {
+
+	mongo.connect(env.mongo_url, (err, db) => {
+		if (err) return res.send("Error connecting to mongodb....");
+
+
+		var con = db.collection("lastsearches");
+
+		con.find({}, {_id: 0}).sort({"time": -1}).toArray((err, i) => {
+			if (err) return res.send("Error connecting to mongodb...");
+
+
+			console.log(i);
+			res.json(i);
+		});
+
+		db.close();
 	});
 
 
 
 
-	res.send("XD");
-});
 
-app.get("/api/latest", (req, res) => {
-	res.send("BANTER");
 });
 
 var server = http.createServer(app).listen(process.env.PORT || PORT, () => {
