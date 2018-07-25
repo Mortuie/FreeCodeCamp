5ba356916c9d6f41e333c241cdb8e406b2329b0d
mortuie <jimmybob-leon@hotmail.co.uk>
2018-07-25 21:18:15 +0100

    Adding a webpack frontend server, handmade. Getting to grips with webpack more and more

---
+++ backend/votingapp/frontend/webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlPlugin = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: './index.html'
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [ HtmlPlugin ]
+};
