eebdbbf03898a259f6f0c34b688efe3721c50db3
mortuie <jimmybob-leon@hotmail.co.uk>
2018-10-03 13:48:34 +0200

    Second pomodoro done, nearly done with the initial redux setup..

--- backend/votingapp/frontend/src/User/Actions.js
+++ backend/votingapp/frontend/src/User/Actions.js
@@ -1,1 +1,36 @@
 import * as Types from './Types';
+import axios from 'axios';
+import { BASE } from '../Constants';
+
+function loginAttempt() {
+    return {
+        type: Types.LOGIN_ATTEMPT,
+    };
+}
+
+function loginSuccess(user) {
+    return {
+        type: Types.LOGIN_SUCCESS,
+        user
+    };
+}
+
+function loginError(error) {
+    return {
+        type: Types.LOGIN_ERROR,
+        error
+    };
+}
+
+export function register(email, password) {
+    return function(dispatch) {
+        dispatch(loginAttempt());
+
+        return axios.post(BASE + '/auth/register', {
+            email,
+            password
+        })
+        .then(res => dispatch(loginSuccess(res.data)))
+        .catch(err => dispatch(loginError(err)));
+    };
+}
--- backend/votingapp/frontend/src/User/Login.js
+++ backend/votingapp/frontend/src/User/Login.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import register from './Actions';
+import { connect } from 'react-redux';
 
-export default class Login extends Component {
+class Login extends Component {
+
+  state = {
+    email: '',
+    password: '',
+  };
+
+  changeState = (e) => {
+    let obj = {};
+    obj[e.target.name] = e.target.value;
+    this.setState(obj);
+  }
+
   render() {
     return (
       <div>
-        <input />
-        <input />
+        <input name="email" value={this.state.email} onChange={this.changeState} />
+        <input name="password" type="password" value={this.state.password} onChange={this.changeState} />
         <button>Submit</button>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return state;
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    register: dispatch(register(this.state.email, this.state.password)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
