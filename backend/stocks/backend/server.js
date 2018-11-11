571a01e0e30d393fc1ee5e7746451a54d901262e
mortuie <jimmybob-leon@hotmail.co.uk>
2018-11-11 21:44:25 +0100

    Working on TDD with all the options for the stocks. It's actually quite fun

--- backend/stocks/backend/server.js
+++ backend/stocks/backend/server.js
@@ -1,8 +1,8 @@
-const initialise = require('./init');
 const chalk = require('chalk');
 const express = require('express');
 const app = express();
 const websocketserver = require('websocket').server;
+const db = require('./init');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -10,27 +10,68 @@ if (process.env.NODE_ENV !== 'production') {
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => console.log('Listening on port: ', PORT));
+async function start() {
+  try {
+    await db.init();
+    console.log(chalk.green('Connected to the Database'));
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
 
-const wsserver = new websocketserver({
-  httpServer: server,
-  autoAcceptConnections: true
-});
-
-wsserver.on('connect', conn => {
-  console.log('Client has connected: ', conn.remoteAddress);
-
-  conn.on('message', message => {
-    console.log('Received: ' + JSON.parse(message.utf8Data));
-    conn.sendUTF(message.utf8Data);
+async function initServers() {
+  const server = app.listen(PORT, () =>
+    console.log('Listening on port: ', PORT)
+  );
+  const wsserver = new websocketserver({
+    httpServer: server,
+    autoAcceptConnections: true
   });
-});
 
-wsserver.on('close', (conn, reason, desc) => {
-  console.log('Client has disconnected: ', conn.remoteAddress);
-});
+  wsserver.on('connect', conn => {
+    console.log('Client has connected: ', conn.remoteAddress);
 
-module.exports = {
-  wsserver,
-  server
-};
+    conn.on('message', message => {
+      const data = JSON.parse(message.utf8Data);
+      switch (data.type) {
+        case 'getAllStocks':
+          console.log('Getting all stocks..');
+          break;
+        case 'addStock':
+          console.log('Adding stock...');
+          break;
+        case 'removeStock':
+          console.log('Removing stock...');
+          break;
+        case 'stockInfo':
+          console.log('Getting stock info...');
+          break;
+        default:
+          console.log('This is the default case..');
+          conn.send(
+            JSON.stringify({
+              type: 'default',
+              error: 'None of the types matched'
+            })
+          );
+          break;
+      }
+    });
+  });
+
+  wsserver.on('close', (conn, reason, desc) => {
+    console.log('Client has disconnected: ', conn.remoteAddress);
+  });
+  return { server, wsserver };
+}
+
+module.exports = (async () => {
+  try {
+    await start();
+    const res = await initServers();
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+})();
--- backend/stocks/backend/test/testwebsocket.js
+++ backend/stocks/backend/test/testwebsocket.js
@@ -1,26 +1,109 @@
 const wsc = require('websocket').client;
-const servers = require('../server');
+var assert = require('chai').assert;
 
 describe('Websocket server tests', function() {
-  it('xd', function() {
-    const client = new wsc();
+  let servers;
+  let client;
+  let clientConn;
+  before(async function() {
+    servers = await require('../server');
+  });
+
+  beforeEach(function(done) {
+    client = new wsc();
 
     client.connect('ws://localhost:3000/');
 
     client.on('connect', conn => {
-      console.log('Connected!!');
-
-      conn.on('message', message => {
-        console.log('Got: ' + message.utf8Data);
-      });
-
-      conn.sendUTF(JSON.stringify({ name: 'leon' }));
+      clientConn = conn;
+      done();
     });
+  });
+
+  it('Testing Default case..', function(done) {
+    const expectedOutput = {
+      type: 'default',
+      error: 'None of the types matched'
+    };
+
+    clientConn.send(JSON.stringify({ name: 'leon' }));
+
+    clientConn.on('message', message => {
+      assert.deepEqual(
+        expectedOutput,
+        JSON.parse(message.utf8Data),
+        'Should equal a default error'
+      );
+      done();
+    });
+  });
+
+  it('Getting all available stocks', function(done) {
+    clientConn.send(JSON.stringify({ type: 'getAllStocks' }));
+
+    clientConn.on('message', message => {
+      const messageDecoded = JSON.parse(message.utf8Data);
+
+      assert.isObject(messageDecoded, 'Should be a JSON object');
+
+      assert.equal(
+        'getAllStocks',
+        messageDecoded.type,
+        'Types should be equal'
+      );
+
+      assert.isArray(
+        messageDecoded.stocks,
+        'Should contain an array of stocks'
+      );
+
+      done();
+    });
+  });
+
+  it('Adding a stock', function(done) {
+    clientConn.send(JSON.stringify({ type: 'addStock' }));
+
+    clientConn.on('message', message => {
+      const decoded = JSON.parse(message.utf8Data);
+
+      assert.isObject(decoded);
+
+      done();
+    });
+  });
+
+  it('Removing a stock', function(done) {
+    clientConn.send(JSON.stringify({ type: 'removeStock', stock: 'APPL' }));
+
+    clientConn.on('message', message => {
+      const decoded = JSON.parse(message.utf8Data);
+
+      assert.isObject(decoded);
+
+      done();
+    });
+  });
+
+  it('Getting stock info', function(done) {
+    clientConn.send(JSON.stringify({ type: 'stockInfo', stock: 'APPL' }));
+
+    clientConn.on('message', message => {
+      const decoded = JSON.parse(message.utf8Data);
+
+      assert.isObject(decoded);
+
+      done();
+    });
+  });
+
+  afterEach(function(done) {
+    clientConn.close();
+    done();
   });
 
   after(function(done) {
     servers.wsserver.shutDown();
-    servers.server.close();
-    done();
+    servers.server.close(done);
   });
 });
