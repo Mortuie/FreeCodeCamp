ca58f5e4e409e593da6fd5f20c6bd5eb31065e72
mortuie <jimmybob-leon@hotmail.co.uk>
2018-11-11 10:53:23 +0100

    Using TDD for the websockets

--- backend/stocks/backend/server.js
+++ backend/stocks/backend/server.js
@@ -10,31 +10,25 @@ if (process.env.NODE_ENV !== 'production') {
 
 const PORT = process.env.PORT || 3000;
 
-
-const server = app.listen(PORT, () => console.log("Listening on port: ", PORT));
-
+const server = app.listen(PORT, () => console.log('Listening on port: ', PORT));
 
 const wsserver = new websocketserver({
   httpServer: server,
   autoAcceptConnections: true
 });
 
-
 wsserver.on('connect', conn => {
-  console.log("CONNECTED...");
-
+  console.log('Client has connected: ', conn.remoteAddress);
 
   conn.on('message', message => {
-    console.log("Received: " + message.utf8Data);
+    console.log('Received: ' + JSON.parse(message.utf8Data));
     conn.sendUTF(message.utf8Data);
   });
 });
 
-
 wsserver.on('close', (conn, reason, desc) => {
-  console.log('CLOSED');
+  console.log('Client has disconnected: ', conn.remoteAddress);
 });
-
 
 module.exports = {
   wsserver,
