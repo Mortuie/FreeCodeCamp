43a06fdd7d1e32da035e9ea268a2aec1155c8e95
mortuie <jimmybob-leon@hotmail.co.uk>
2018-12-08 21:53:06 +0100

    Getting foreign keys working

--- backend/todos/backend/migrations/20181207173652_initial.js
+++ backend/todos/backend/migrations/20181207173652_initial.js
@@ -3,10 +3,24 @@ exports.up = function(knex, Promise) {
     knex.schema.createTable('users', table => {
       table.increments('id');
       table.string('name').unique();
+    }),
+    knex.schema.createTable('todos', table => {
+      table.increments('id');
+      table.string('title').unique();
+      table.boolean('completed').default(false);
+      table
+        .integer('userId')
+        .unsigned()
+        .index()
+        .references('id')
+        .inTable('users');
     })
   ]);
 };
 
 exports.down = function(knex, Promise) {
-  return Promise.all([knex.schema.dropTable('users')]);
+  return Promise.all([
+    knex.schema.dropTable('users'),
+    knex.schema.dropTable('todos')
+  ]);
 };
--- backend/todos/backend/seeds/users.js
+++ backend/todos/backend/seeds/users.js
@@ -2,10 +2,32 @@ exports.seed = (knex, Promise) => {
   return knex('users')
     .del()
     .then(() => {
-      return knex('users').insert([
-        { name: 'Leon' },
-        { name: 'Bob' },
-        { name: 'Chelsea' }
-      ]);
+      return knex('users')
+        .insert([{ name: 'Leon' }, { name: 'Bob' }, { name: 'Chelsea' }])
+        .then(() => {
+          return knex('todos')
+            .del()
+            .then(() => {
+              return knex
+                .from('users')
+                .select('id')
+                .where({ name: 'Leon' })
+                .then(rows => {
+                  console.log('HERE: ', rows);
+                  console.log(rows[0].id);
+                  return knex('todos').insert([
+                    {
+                      title: 'Clean kitchen',
+                      userId: rows[0].id
+                    },
+                    {
+                      title: 'Go Shopping',
+                      userId: rows[0].id
+                    },
+                    { title: 'Find shoes', userId: rows[0].id }
+                  ]);
+                });
+            });
+        });
     });
 };
