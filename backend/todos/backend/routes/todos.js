f1f4ff24413f4075b738ece7af6d56eb4087018a
mortuie <jimmybob-leon@hotmail.co.uk>
2018-12-10 21:26:07 +0100

    Added the rest api for the backend

--- backend/todos/backend/routes/todos.js
+++ backend/todos/backend/routes/todos.js
@@ -20,9 +20,50 @@ module.exports = (app, knex) => {
       });
   });
 
-  app.post('/api/v1/todo', (req, res) => {});
+  app.post('/api/v1/todo', (req, res) => {
+    knex('todos')
+      .insert({ title: req.body.title, userId: req.body.id })
+      .then(ress => {
+        console.log(ress);
+        res.send('Hello World!');
+      })
+      .catch(err => {
+        console.log(err);
+        if (err.code === '23505') {
+          return res.json({
+            err: true,
+            message: 'Duplicate title. Please choose another todo title...'
+          });
+        } else {
+          return res.json({ err: true, message: 'Unknown...' });
+        }
+      });
+  });
 
-  app.patch('/api/v1/todo/:id', (req, res) => {});
+  app.patch('/api/v1/todo/:id', (req, res) => {
+    console.log('params:', req.params);
+    console.log('body:', req.body);
+
+    let updated = { completed: false };
+
+    if (req.body.title) {
+      updated.title = req.body.title;
+    }
+
+    if (req.body.completed) {
+      updated.completed = true;
+    }
+
+    console.log(updated);
+
+    knex('todos')
+      .where({ id: req.params.id })
+      .update(updated)
+      .then(ress => {
+        console.log(ress);
+        return res.json({ status: ress });
+      });
+  });
 
   app.delete('/api/v1/todo/:id', (req, res) => {
     knex
