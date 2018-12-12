7e2747e2e5ac2d945be666595c886b8ff608d1e1
mortuie <jimmybob-leon@hotmail.co.uk>
2018-12-12 20:40:51 +0100

    Initial commit for the book trading club

---
+++ backend/booktrading/backend/knexfile.js
@@ -0,0 +1,40 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'postgres',
+      user: 'postgres',
+      password: 'root'
+    }
+  },
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
