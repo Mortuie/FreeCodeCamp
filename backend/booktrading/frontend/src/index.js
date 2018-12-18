58efc7f667e22f0ec0da3bb9488d9e6b4511c6bd
mortuie <jimmybob-leon@hotmail.co.uk>
2018-12-18 21:32:18 +0100

    Using CRA instead and then adding hooks

--- backend/booktrading/frontend/src/index.js
+++ backend/booktrading/frontend/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-function App() {
-  return <div>Hello World</div>;
-}
-
-ReactDOM.render(App, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
