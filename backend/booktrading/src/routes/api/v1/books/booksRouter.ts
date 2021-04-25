fecfae003cd4257740bc17cb7cca68201cc8189e
mortuie <jimmybob-leon@hotmail.co.uk>
2021-04-26 00:08:50 +0100

    Working on the booksRouter

--- backend/booktrading/src/routes/api/v1/books/booksRouter.ts
+++ backend/booktrading/src/routes/api/v1/books/booksRouter.ts
@@ -3,17 +3,30 @@ import { User } from "../../../../types/user";
 import { isLoggedIn } from "../../../../utils/auth";
 import { query } from "../../../../utils/db";
 
+enum RESOURCE {
+  BOOK = "Book",
+}
+
+interface Error {
+  message: string;
+  type: string;
+}
+
+const NOT_FOUND_ERROR = (type: RESOURCE, book_uuid: string): Error => ({
+  message: `${type} with uuid: ${book_uuid} not found`,
+  type: "RESOURCE_NOT_FOUND",
+});
+
+const BAD_REQUEST_ERROR: Error = {
+  message: "Bad request input",
+  type: "BAD_REQUEST_INPUT",
+};
+
 const booksRouter = Router();
 
 booksRouter.get("/", async (req, res) => {
-  const { rows } = await query("SELECT * FROM books;");
-
-  res.json(rows);
-});
-
-booksRouter.get("/users", async (req, res) => {
   const { rows } = await query(
-    "SELECT b.uuid as books_uuid, b.name, b.picture, b.created_at, u.fullname, u.uuid as users_uuid FROM books b JOIN users u ON b.user_uuid = u.uuid;"
+    "SELECT b.*, u.fullname, u.picture as user_picture  FROM books b JOIN users u ON b.user_uuid = u.uuid;"
   );
 
   res.json(rows);
@@ -23,40 +36,21 @@ booksRouter.get("/:book_uuid", async (req, res) => {
   const { book_uuid } = req.params;
 
   try {
-    const { rows } = await query("SELECT * FROM books WHERE uuid = $1;", [
-      book_uuid,
-    ]);
-
-    if (!rows.length) {
-      return res.json({});
-    }
-
-    res.json(rows[0]);
-  } catch (err) {
-    console.log(err);
-    return res.json({});
-  }
-});
-
-booksRouter.get("/:book_uuid/users", async (req, res) => {
-  const { book_uuid } = req.params;
-
-  try {
     const {
       rows,
     } = await query(
-      "SELECT b.uuid, b.name, b.picture, b.created_at, u.fullname FROM books b JOIN users u ON b.user_uuid = u.uuid WHERE b.uuid = $1;",
+      "SELECT b.*, u.fullname, u.picture as user_picture  FROM books b JOIN users u ON b.user_uuid = u.uuid WHERE b.uuid = $1;",
       [book_uuid]
     );
 
     if (!rows.length) {
-      return res.json({});
+      return res.status(404).json(NOT_FOUND_ERROR(RESOURCE.BOOK, book_uuid));
     }
 
     res.json(rows[0]);
   } catch (err) {
     console.log(err);
-    return res.json({});
+    return res.status(400).json(BAD_REQUEST_ERROR);
   }
 });
 
@@ -66,9 +60,7 @@ booksRouter.post("/", isLoggedIn, async (req, res) => {
   const uuid = user.uuid;
 
   if (!name) {
-    res
-      .status(400)
-      .json({ message: "Bad request input", type: "BAD_REQUEST_INPUT" });
+    res.status(400).json(BAD_REQUEST_ERROR);
   }
 
   let rows;
@@ -109,17 +101,12 @@ booksRouter.patch("/:book_uuid", isLoggedIn, async (req, res) => {
     const { rows } = await query(sqlQuery, [book_uuid, uuid]);
 
     if (!rows.length) {
-      return res.status(404).json({
-        message: `Book with uuid: ${book_uuid} not found`,
-        type: "RESOURCE_NOT_FOUND",
-      });
+      return res.status(404).json(NOT_FOUND_ERROR(RESOURCE.BOOK, book_uuid));
     } else {
       return res.status(200).json(rows[0]);
     }
   } catch (err) {
-    res
-      .status(400)
-      .json({ message: "Bad request input", type: "BAD_REQUEST_INPUT" });
+    res.status(400).json(BAD_REQUEST_ERROR);
   }
 });
 
@@ -137,17 +124,12 @@ booksRouter.delete("/:book_uuid", isLoggedIn, async (req, res) => {
     );
 
     if (!rows.length) {
-      return res.status(404).json({
-        message: `Book with uuid: ${book_uuid} not found`,
-        type: "RESOURCE_NOT_FOUND",
-      });
+      return res.status(404).json(NOT_FOUND_ERROR(RESOURCE.BOOK, book_uuid));
     } else {
       return res.status(200).json({ message: "success", type: "SUCCESS" });
     }
   } catch (err) {
-    res
-      .status(400)
-      .json({ message: "Bad request input", type: "BAD_REQUEST_INPUT" });
+    res.status(400).json(BAD_REQUEST_ERROR);
   }
 });
 
