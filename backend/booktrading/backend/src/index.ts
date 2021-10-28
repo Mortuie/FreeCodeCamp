0fb3a540b5e4fab9a8cb19d2b539fbcbed801486
mortuie <jimmybob-leon@hotmail.co.uk>
2021-10-28 01:37:28 +0100

    Adding code

--- backend/booktrading/backend/src/index.ts
+++ backend/booktrading/backend/src/index.ts
@@ -1,12 +1,11 @@
-import { parseEnvVars } from "./utils/envVars";
 import express from "express";
 import cookieParser from "cookie-parser";
 import { parseSignedCookies } from "./middleware";
 import { getV1Router } from "./routes/v1";
 import cors from "cors";
+import { parsedEnvVars } from "./utils/envVars";
 
 const app = express();
-const envVars = parseEnvVars();
 
 app.use(
   cors({
@@ -15,12 +14,12 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(cookieParser(envVars.COOKIE_SECRET));
+app.use(cookieParser(parsedEnvVars.COOKIE_SECRET));
 
 app.use(parseSignedCookies);
 
-app.use("/api/v1", getV1Router(envVars));
+app.use("/api/v1", getV1Router());
 
-app.listen(envVars.PORT, () => {
-  console.log(`Listening on port: ${envVars.PORT}`);
+app.listen(parsedEnvVars.PORT, () => {
+  console.log(`Listening on port: ${parsedEnvVars.PORT}`);
 });
--- backend/booktrading/backend/src/routes/v1/index.ts
+++ backend/booktrading/backend/src/routes/v1/index.ts
@@ -1,14 +1,13 @@
 import { Router } from "express";
-import { EnvVarsType } from "../../utils/envVars";
 import { getV1BookRouter } from "./books";
 import { getV1TradesRouter } from "./trades";
 import { getV1UserRouter } from "./users";
 
-const getV1Router = (envVars: EnvVarsType): Router => {
+const getV1Router = (): Router => {
   const v1Router = Router();
 
   v1Router.use("/books", getV1BookRouter());
-  v1Router.use("/users", getV1UserRouter(envVars));
+  v1Router.use("/users", getV1UserRouter());
   v1Router.use("/trades", getV1TradesRouter());
 
   return v1Router;
