e9a8168409743d64b0ab1bb75a9a0240bb8bea03
mortuie <jimmybob-leon@hotmail.co.uk>
2022-10-18 23:21:43 +0100

    Started pinterest clone

---
+++ backend/pinterestclone/backend/src/test/java/com/mortuie/pinterest/PinterestClone/PinterestCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mortuie.pinterest.PinterestClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinterestCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
