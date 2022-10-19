cfd72e4926b2a96f4e97af4f9da4de5e3783a5fb
mortuie <jimmybob-leon@hotmail.co.uk>
2022-10-19 23:44:00 +0100

    Moved away from deprecated extensions...

--- backend/pinterestclone/backend/src/main/java/com/mortuie/pinterest/PinterestClone/controllers/TestController.java
+++ backend/pinterestclone/backend/src/main/java/com/mortuie/pinterest/PinterestClone/controllers/TestController.java
@@ -1,7 +1,13 @@
 package com.mortuie.pinterest.PinterestClone.controllers;
 
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 public class TestController {
@@ -13,7 +19,18 @@ public class TestController {
 
 
     @GetMapping("/testing")
-    public String testing() {
-        return "memes";
+    public String testing(Principal principal) {
+        String name = principal.getName();
+        return "memes" + name;
+    }
+
+    @GetMapping("/test")
+    public Object test(@AuthenticationPrincipal DefaultOAuth2User user) {
+        return user;
+    }
+
+    @GetMapping("/api/testing")
+    public String testing2() {
+        return "please help me";
     }
 }
